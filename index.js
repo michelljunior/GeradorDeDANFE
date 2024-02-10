259125147be6106fcca90907e5c41edd77c2cfa4
Michel Anderson Lourenço Junior <michelaljunior@outlook.com>
2024-02-09 22:03:32 -0300

    Ultima melhoria PDF

--- index.js
+++ index.js
@@ -30,13 +30,13 @@ function GerarCodigoDeBarras (text) {
             let gifBase64 = `data:image/gif;base64,${buffer.toString('base64')}`
             htmlPdf = htmlPdf.replace(/CODIGO_DE_BARRAS/gi, gifBase64)
 
-            // fs.writeFile('text.html', htmlPdf, (err) => {
-            //   if (err) {
-            //     console.error('Ocorreu um erro ao salvar o arquivo:', err);
-            //     return;
-            //   }
-            //   console.log('Texto salvo com sucesso!');
-            // });
+            fs.writeFile('text.html', htmlPdf, (err) => {
+              if (err) {
+                console.error('Ocorreu um erro ao salvar o arquivo:', err);
+                return;
+              }
+              console.log('Texto salvo com sucesso!');
+            });
 
             (async () => {
 
@@ -55,10 +55,7 @@ function GerarCodigoDeBarras (text) {
                 path: pdfPath,
                 format: 'A4',
                 displayHeaderFooter: true,
-                footerTemplate: `
-                  <div id="footer-template" style="font-size:10px !important; color:#808080; padding-left:50px">
-                    Documento gerado as <span class="date"></span> por <a href="https://datasynchro.com.br">DataSynchro</a>
-                  </div>`,
+                footerTemplate: `<div id="footer-template" style="font-size:10px !important; color:#808080; padding-left:50px">Documento gerado por <a href="https://datasynchro.com.br">DataSynchro</a></div>`,
                 margin: {
                   top: '30px',
                   bottom: '30px',
